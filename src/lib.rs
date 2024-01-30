8bbfcf00fe33837c2b896177dfa7ac2516180efb
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2024-01-30 23:27:29 +0100

    chore: Add initial implementation to convert windows host path to WSL

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,51 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Copyright (c) 2024 Jan Holthuis <jan.holthuis@rub.de>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
+// of the MPL was not distributed with this file, You can obtain one at
+// http://mozilla.org/MPL/2.0/.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+use typed_path::{Utf8UnixPathBuf, Utf8WindowsComponent, Utf8WindowsPath, Utf8WindowsPrefix};
+
+#[derive(Debug)]
+pub enum Error {
+    RelativePathError,
+    InvalidPrefixError,
+}
+
+pub fn windows_to_wsl(windows_path: &str) -> Result<String, Error> {
+    let path = Utf8WindowsPath::new(windows_path);
+    if !path.is_absolute() {
+        return Err(Error::RelativePathError);
+    }
+
+    // "C:\foo" (6 chars) -> "/mnt/c/foo" (10 chars)
+    let expected_length = windows_path.len() + 4;
+    let mut output = Utf8UnixPathBuf::with_capacity(expected_length);
+    for component in path.components() {
+        match component {
+            Utf8WindowsComponent::Prefix(prefix_component) => match prefix_component.kind() {
+                Utf8WindowsPrefix::VerbatimDisk(disk) => {
+                    output.push("/mnt");
+                    output.push(disk.to_ascii_lowercase().to_string());
+                }
+                Utf8WindowsPrefix::Disk(disk) => {
+                    output.push("/mnt");
+                    output.push(disk.to_ascii_lowercase().to_string());
+                }
+                _ => {
+                    return Err(Error::InvalidPrefixError);
+                }
+            },
+            Utf8WindowsComponent::RootDir => (),
+            Utf8WindowsComponent::CurDir => output.push("."),
+            Utf8WindowsComponent::Normal(name) => output.push(name),
+            Utf8WindowsComponent::ParentDir => output.push(".."),
+        };
+    }
+
+    Ok(output.into_string())
 }
 
 #[cfg(test)]
@@ -7,8 +53,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_windows_to_wsl() {
+        assert_eq!(windows_to_wsl("C:\\Windows").unwrap(), "/mnt/c/Windows");
+        assert_eq!(
+            windows_to_wsl("C:\\foo\\..\\bar\\.\\baz.txt").unwrap(),
+            "/mnt/c/foo/../bar/./baz.txt"
+        );
+        assert_eq!(
+            windows_to_wsl("C:\\Program Files (x86)\\Foo\\bar.txt").unwrap(),
+            "/mnt/c/Program Files (x86)/Foo/bar.txt"
+        );
+    }
+
+    #[test]
+    fn test_windows_to_wsl_unc() {
+        assert_eq!(
+            windows_to_wsl("\\\\?\\C:\\Windows").unwrap(),
+            "/mnt/c/Windows"
+        );
+        assert_eq!(
+            windows_to_wsl("\\\\?\\C:\\foo\\..\\bar\\.\\baz.txt").unwrap(),
+            "/mnt/c/foo/../bar/./baz.txt"
+        );
+        assert_eq!(
+            windows_to_wsl("\\\\?\\C:\\Program Files (x86)\\Foo\\bar.txt").unwrap(),
+            "/mnt/c/Program Files (x86)/Foo/bar.txt"
+        );
     }
 }
